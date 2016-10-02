dd053d49892e98fbe87ac84283707fddb5af1cf4
yzhou65 <yzhou65@hawk.iit.edu>
2016-10-02 18:43:23 -0500

    2nd commit

--- leet/src/com/medium/EvaluateReversePolishNotation.java
+++ leet/src/com/medium/EvaluateReversePolishNotation.java
@@ -10,19 +10,19 @@ public class EvaluateReversePolishNotation {
         for (String token : tokens) {
 			if (!operators.contains(token)) {
 				s.push(Integer.parseInt(token));
-				continue;
 			}
-			
-			int operand1 = s.pop();
-			int operand2 = s.pop();
-			if (token.equals("+")) {
-				s.push(operand2 + operand1);
-			} else if (token.equals("-")) {
-				s.push(operand2 - operand1);
-			} else if (token.equals("*")) {
-				s.push(operand2 * operand1);
-			} else if (token.equals("/")) {
-				s.push(operand2 / operand1);
+			else {
+				int operand1 = s.pop();
+				int operand2 = s.pop();
+				if (token.equals("+")) {
+					s.push(operand2 + operand1);
+				} else if (token.equals("-")) {
+					s.push(operand2 - operand1);
+				} else if (token.equals("*")) {
+					s.push(operand2 * operand1);
+				} else if (token.equals("/")) {
+					s.push(operand2 / operand1);
+				}
 			}
 		}
         return s.pop();
